502421a8f530b953ba6e16536d5d916a005a0a20
Jeff Terrell <jeffaterrell@gmail.com>
2021-12-08 14:42:27 -0500

    add logic for passing tests 4&5
    Co-authored-by: Anna Pittman <annaepittman@gmail.com>
    Co-authored-by: Kate Kiatsiri <keidsiri@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,12 +1,25 @@
+using System.Collections.Generic; // if we need to use the list objects.
+
 namespace ToDoList.Models
 {
   public class Item
   {
     public string Description { get; set;} // public string Description = ""; ***this also works instead of using "get" and is much simpler logic***
+    private static List<Item> _instances = new List<Item> {};
     public Item(string description)
     {
       Description = description;
+      _instances.Add(this);
+    }
+
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
-
