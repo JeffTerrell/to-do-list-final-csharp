7505e968c54b4a6d203dd8c3c4a7a70c7c7e4338
Jeff Terrell <jeffaterrell@gmail.com>
2021-12-08 13:44:14 -0500

    add logic for passing test 3
    Co-authored-by: Kate Kiatsiri <keidsiri@gmail.com>
    Co-authored-by: Anna Pittman <annaepittman@gmail.com>

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -22,5 +22,21 @@ namespace ToDoList.Tests
       string result = newItem.Description;
       Assert.AreEqual(description, result);
     }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
   }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -2,10 +2,11 @@ namespace ToDoList.Models
 {
   public class Item
   {
-    public string Description { get; }
+    public string Description { get; set;} // public string Description = ""; ***this also works instead of using "get" and is much simpler logic***
     public Item(string description)
     {
       Description = description;
     }
   }
 }
+
