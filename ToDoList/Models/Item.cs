7e2ef9d862a5f37588b389049aed25e05c105b67
Jeff Terrell <jeffaterrell@gmail.com>
2021-12-08 13:11:10 -0500

    add files and folders and first passing test
    Co-authored-by: Kate Kiatsiri <keidsiri@gmail.com>
    Co-authored-by: Anna Pittman <annaepittman@gmail.com>

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,23 @@
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public string Description { get; }
+    public Item(string description)
+    {
+      Description = description;
+    }
+  }
+}
+
+
+
+namespace ToDoList.Models
+{
+  public class Item
+  {
+
+    // properties, methods, etc. will go here.
+
+  }
+}
