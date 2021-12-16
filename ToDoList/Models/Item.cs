badca8e3c4a6b713a5b1946a4b11ab7db3846b16
Jeff Terrell <jeffaterrell@gmail.com>
2021-12-16 15:44:29 -0500

    create link to new show.html to view object in list
    Co-authored-by: Anna Pittman <annaepittman@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,11 +5,13 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description = ""; //{ get; set;} // public string Description = ""; ***this also works instead of using "get" and is much simpler logic***
+    public int Id { get; }
     private static List<Item> _instances = new List<Item> {};
     public Item(string description)
     {
       Description = description;
       _instances.Add(this);
+      Id = _instances.Count;
     }
 
     public static List<Item> GetAll()
@@ -20,6 +22,11 @@ namespace ToDoList.Models
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+
+    public static Item Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
   }
 }
