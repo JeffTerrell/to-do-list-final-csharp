00a5f2b3cf6e1d036275cf12490eb4389f89ebd1
Jeff Terrell <jeffaterrell@gmail.com>
2021-12-16 14:43:55 -0500

    add ItemsController and Items dir within views
    Co-authored-by: Anna Pittman <annaepittman@gmail.com>

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -13,18 +13,5 @@ namespace ToDoList.Controllers
       List<Item> allItems = Item.GetAll();
       return View(allItems);
     }
-
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
-    {
-      return View();
-    }
-
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
-    }
   }
 }
